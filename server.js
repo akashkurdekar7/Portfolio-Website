2385b92eb8e1fcd6ff4954d13ac0ab82a4a75b61
Akash Kurdekar <akashkurdekar39@gmail.com>
2023-08-24 01:09:07 +0530

    trying to connect it with nodejs

---
+++ server.js
@@ -0,0 +1,30 @@
+const http =require('http')
+const fs = require('fs')
+const port= 4000
+
+const server = http.createServer(function(req,res){
+
+    res.writeHead(200,{'Content-Type ': 'text/html'})
+    fs.readFile('index.html', function(error, data){
+        if (error) {
+            res.writeHead(404)
+            res.write('error 404: file not found')
+        }
+        else{
+            res.write(data)
+        }
+        res.end()
+    })
+
+// res.write("hello this is from node")
+// res.end()
+})
+
+server.listen(port,function(error){
+    if(error){
+        console.log("something went wrong"+ error)
+    }
+    else{
+        console.log("server is listening "+ port)    
+    }
+})
